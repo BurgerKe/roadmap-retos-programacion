aa6f3a5c284348d97b8c1cee0b0467f5861d6dfe
BurgerKe <devkevham@gmail.com>
2024-08-11 11:32:38 -0500

    Create BurgerKe.py

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/BurgerKe.py
@@ -0,0 +1,24 @@
+# Hacemos un comentario simplre con "#".(de una linea)
+# URL sitio web https://www.python.org/
+
+""" 
+Utilizando tres comillas para abrir y cerrar el comentrario
+tambien podemos dejar comentarios. Estos pueden ser de varias lineas.
+"""
+
+#Asi se declara una variable
+my_variable = "Mi variable" 
+my_variable = "Esto reemplaza el valor de la variable" 
+
+#Asi se "declara" una constante
+MY_CONSTANTE = "Hola Mundo" #Se escribe todo en mayúsculas para saber que este valor es fijo y no debe camnbiar. 
+
+"""Tipos de datos primitivos en una variable"""
+my_string = "Mi texto en Pyhton" #Esto es un "string" o cadena de texto con doble comillas.
+my_string = 'Mi otro texto en Python' #Esto es un 'string' o cadena de texto pero con comillas simples.
+a = 1 #Esto es un tipo de dato "int" que representa números enteros.
+b = 2.5 #Esto es un tipo de dato "float" que representa números decimales.
+my_boolean = False #Esto es un "bool" son datos binarios que sirve para condicionales y comparaciones.
+my_boolean = True #Esto es un "bool" son datos binarios que sirve para condicionales y comparaciones.
+
+print ('¡Hola, Python!')
